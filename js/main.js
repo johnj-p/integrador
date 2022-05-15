01188b25b17afb94f76d9c2b816d1f160a7f7680
johnj-p <jojairop@gmail.com>
2022-05-15 12:25:31 -0300

    Segundo commit

--- js/main.js
+++ js/main.js
@@ -6,5 +6,5 @@ function precioTotal(){
     let categoriaVenta = parseInt(document.getElementById("categoria").value);
     let cantidadVenta = parseInt(document.getElementById("cantidad").value);
     let total = (precioVenta*cantidadVenta)-((precioVenta*cantidadVenta)*(categoriaVenta/100));
-    document.getElementById("totalPagar").innerHTML=(`Total a pagar $${total}`);
+    document.getElementById("totalPagar").innerHTML=(`Total a Pagar: $${total}`);
 }
